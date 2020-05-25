97995a7ae37cdbbe12d2d50f3e3cdc019738b329
daijianhua <1763237102@qq.com>
2020-05-25 18:37:43 +0800

    去除无用代码

--- src/views/archivesInfoManagement/index.vue
+++ src/views/archivesInfoManagement/index.vue
@@ -25,7 +25,8 @@
                 <a-col :md="8" :sm="24">
                   <div style="float: right">
                     <a-button style="margin-right: 8px" @click="resetQuery">重置</a-button>
-                    <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button>
+                    <a-button type="primary">查询</a-button>
+                    <!-- <a-button type="primary" @click="$refs.table.refresh(true)">查询</a-button> -->
                   </div>
                 </a-col>
               </a-row>
@@ -52,20 +53,6 @@ import { STable, Ellipsis } from '@/components'
 import { treeData } from './data.js'
 import RiverAndStall from './components/riverAndStall'
 import RiverAndPolicy from './components/riverAndPolicy'
-const statusMap = {
-  1: {
-    status: 'default',
-    text: '未制定方案'
-  },
-  2: {
-    status: 'processing',
-    text: '已制定方案'
-  },
-  3: {
-    status: 'success',
-    text: '已完成'
-  }
-}
 
 export default {
   name: 'ArchivesInfoManagement',
@@ -75,86 +62,15 @@ export default {
       this.$router.push({ path: '/task/solution', query: { taskId: record.id, taskName: record.name } })
     },
     resetQuery () {
-      this.queryParam.status = ''
-      this.$refs.table.refresh(true)
-    },
-    handleEditOrNew (record) {
-      // this.$refs.taskModule.showModal(record)
-    },
-    handleDel (record) {
-      this.$confirm({
-        title: '删除操作',
-        content: `确定要删除${record.name}吗`,
-        onOk () {},
-        onCancel () {}
-      })
+      // this.queryParam.status = ''
+      // this.$refs.table.refresh(true)
     }
   },
   created () {},
   data () {
     return {
       checkedKeys: [],
-      queryParam: {
-        name: '',
-        status: ''
-      },
-      columns: [
-        {
-          title: '序号',
-          scopedSlots: { customRender: 'serial' }
-        },
-        {
-          title: '河湖名称',
-          dataIndex: 'name',
-          scopedSlots: { customRender: 'name' }
-        },
-        {
-          title: '标题',
-          dataIndex: 'site'
-        },
-        {
-          title: '附件',
-          dataIndex: 'principal'
-        },
-        {
-          title: '发布人',
-          dataIndex: 'person'
-        },
-        {
-          title: '时间',
-          dataIndex: 'create_at'
-          // sorter: true
-        },
-        {
-          title: '状态',
-          dataIndex: 'status',
-          scopedSlots: { customRender: 'status' }
-        },
-        {
-          title: '操作',
-          dataIndex: 'action',
-          width: '160px',
-          scopedSlots: { customRender: 'action' }
-        }
-      ],
-      loadData: parameter => {
-        return [{ name: 'sss' }]
-        // return getTasks(Object.assign(parameter, this.queryParam))
-        //   .then(res => {
-        //     return res.data
-        //   })
-      },
-      selectedRowKeys: [],
-      selectedRows: [],
       treeData: treeData
-    }
-  },
-  filters: {
-    statusFilter (type) {
-      return statusMap[type].text
-    },
-    statusTypeFilter (type) {
-      return statusMap[type].status
     }
   }
 }
