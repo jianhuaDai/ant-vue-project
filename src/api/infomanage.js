f8eb0d8cbbb05323e53666af7a24caeff262fea9
daijianhua <1763237102@qq.com>
2020-06-02 09:46:20 +0800

    新增档案信息管理api接口

--- src/api/infomanage.js
+++ src/api/infomanage.js
@@ -1,32 +1,80 @@
 import { axios } from '@/utils/request'
 
 const api = {
-    getarea: '/regionalism/get_regionalisms',
-    gethehuxinxi: '/water_info/get_water_infos',
-    gethehuzhangxinxi: '/staff/get_staffs'
+  ADDARCHIVESINFO: '/documents',
+  DELETEARCHIVESINFO: '/documents/delete',
+  GETARCHIVESINFOLIST: '/documents/page_query'
 }
-export default api
-// 行政区域查询
-export function getArea () {
+// 添加档案信息
+/**
+ *
+ * @param {
+ attachment_url	string
+附件链接
+author	string
+发布人
+create_at	string
+创建时间
+delete_at	string
+删除时间
+document_id	string
+档案编码
+regionalism_id	string
+行政区划代码
+title	string
+标题
+update_at	string
+更新时间
+version	integer
+版本
+water_id	string
+河湖编码
+type	integer
+档案类型:1,一河一档 2,一河一策
+ * }
+ */
+export function addArchivesInfo (params) {
+  return axios({
+    url: api.ADDARCHIVESINFO,
+    method: 'post',
+    data: params
+  })
+}
+// 删除档案（逻辑删除）
+export function deleteArchivesInfo (id) {
     return axios({
-        url: api.getarea,
-        method: 'get'
-        // params: parameter
+        url: `${api.DELETEARCHIVESINFO}/${id}`,
+        method: 'post'
     })
 }
-// 查询河湖信息
-export function getHehuxinxi (parameter) {
+
+// 分页查询档案信息
+/**
+ * @param {
+ * id	string
+ 主键编码
+ keyword	string
+ 关键词查询
+ page	integer
+ 页数
+ page_size	integer
+ 页大小
+ regionalism_id	string
+ 行政区划代码
+ status	integer
+ 状态1启用0禁用
+ type	integer
+档案类型:1,一河一档 2,一河一策
+ title	string
+ 标题
+ water_name	string
+ 河湖名称
+ * } params
+ */
+export function getArchivesList (params) {
     return axios({
-        url: api.gethehuxinxi,
-        method: 'get',
-        params: parameter
-    })
-}
-// 查询河湖长信息
-export function getHehuzhangxinxi (parameter) {
-    return axios({
-        url: api.gethehuzhangxinxi,
-        method: 'get',
-        params: parameter
+        url: api.GETARCHIVESINFOLIST,
+        method: 'post',
+        data: params
     })
 }
