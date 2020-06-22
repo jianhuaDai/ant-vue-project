f71ce8582e9502c65f7d3379d767e49efd5c227b
lidayong@ieforever.com <lidayong@ieforever.com>
2020-06-22 15:28:57 +0800

    微调

--- src/views/map/OneMap.vue
+++ src/views/map/OneMap.vue
@@ -312,7 +312,7 @@ export default {
           detailModal: 'lakeRiverDetail',
           detailTitle: '河湖详情'
         }
-        _this.markerClick('id', layerItem)
+        // _this.markerClick('id', layerItem)
         // this.$refs.detailModal.showModal('', { detailModal: 'lakeRiverDetail' }, 'sssss')
       })
       Map.addSource(sourceName + 'text', {
